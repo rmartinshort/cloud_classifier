c3b2eb008f6c7b40cd2252e1016304ebfbc5da6a
Robert <rmartin-short@berkeley.edu>
2022-05-28 21:02:17 +0100

    Make it work on gpu

--- cloud_classifier/model/utils.py
+++ cloud_classifier/model/utils.py
@@ -13,19 +13,18 @@ def set_transform():
             transforms.RandomVerticalFlip(p=0.5)
         ]),
         transforms.RandomGrayscale(p=0.2),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.ToTensor()
     ])
 
     return transform_pipe
 
 
-def evaluation_metrics(model ,inputs ,labels ,threshold=0.5):
+def evaluation_metrics(model, inputs, labels, threshold=0.5):
 
     outputs = model(inputs)
-    outputs = torch.sigmoid(outputs).detach().numpy()
+    outputs = torch.sigmoid(outputs).detach().cpu().numpy()
     predictions = np.where(outputs>threshold, 1, 0)
-    target = labels.detach().numpy()
+    target = labels.detach().cpu().numpy()
 
     # print these out
     # precision_score(predicted_op,true_op,average=None)
