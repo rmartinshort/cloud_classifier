a914f711d73b02a4579659197a00fa974d11a857
Robert <rmartin-short@berkeley.edu>
2022-05-28 21:46:52 +0100

    Make it work on gpu

--- cloud_classifier/model/utils.py
+++ cloud_classifier/model/utils.py
@@ -9,8 +9,7 @@ def set_transform():
         transforms.ToPILImage(mode="RGB"),
         transforms.RandomChoice([
             transforms.RandomRotation(degrees=(-30, 30)),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomVerticalFlip(p=0.5)
+            transforms.RandomHorizontalFlip(p=0.5)
         ]),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -19,19 +18,29 @@ def set_transform():
     return transform_pipe
 
 
-def evaluation_metrics(model ,inputs ,labels , threshold=0.5):
+def evaluation_metrics(model, inputs, labels, classes, threshold=0.5):
+    """
 
+    :param model:
+    :param inputs:
+    :param labels:
+    :param classes:
+    :param threshold:
+    :return:
+    """
     outputs = model(inputs)
     outputs = torch.sigmoid(outputs).detach().cpu().numpy()
-    predictions = np.where(outputs>threshold, 1, 0)
+    predictions = np.where(outputs > threshold, 1, 0)
     target = labels.detach().cpu().numpy()
 
-    # print these out
-    # precision_score(predicted_op,true_op,average=None)
-    # recall_score(predicted_op,true_op,average=None)
-    # f1_score(predicted_op,true_op,average=None)
-    overall_f1 = f1_score(predictions,
-                          target,
-                          average="micro")
+    precision_array = precision_score(predictions, target, average=None)
+    recall_array = recall_score(predictions, target, average=None)
+    f1_array = f1_score(predictions, target, average=None)
+
+    print("-------------------------------------")
+    for class_name, p, r, f1 in zip(classes, precision_array, recall_array, f1_array):
+        print("{}: Pr: {} Re: {} F1: {}".format(class_name, p, r, f1))
+
+    overall_f1 = f1_score(predictions, target, average="micro")
 
     return overall_f1
